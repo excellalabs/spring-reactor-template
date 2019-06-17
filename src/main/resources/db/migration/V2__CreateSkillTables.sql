93b2d8949050b15f08bd94e260946a61266bedd5
Dale Gartman <dale.gartman@excella.com>
2019-06-17 16:03:22 -0400

    Add infrastructure for separate skill category relation

--- src/main/resources/db/migration/V2__CreateSkillTables.sql
+++ src/main/resources/db/migration/V2__CreateSkillTables.sql
@@ -1,13 +1,22 @@
+CREATE TABLE skill_category (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
 CREATE TABLE skill (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    category VARCHAR(100)
+    name VARCHAR(100) NOT NULL,
+    category_id INTEGER
 );
+
+ALTER TABLE skill
+ADD CONSTRAINT fk_skill_skcat
+FOREIGN KEY (category_id) REFERENCES skill_category(id);
 
 CREATE TABLE employee_skill (
     employee_id INTEGER REFERENCES employee(id) ON DELETE CASCADE,
     skill_id INTEGER REFERENCES skill(id) ON DELETE CASCADE,
-    proficiency VARCHAR(16),
-    is_primary BOOLEAN,
+    proficiency VARCHAR(16) NOT NULL,
+    is_primary BOOLEAN NOT NULL,
     PRIMARY KEY (employee_id, skill_id)
 );
