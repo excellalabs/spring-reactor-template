5a862a839fc5d8b210ef1db2f54a150c5d307b8d
Kenneth Russell <kenrusselldc@gmail.com>
2019-06-26 15:18:03 -0400

    CORS fix

--- src/main/java/com/excella/reactor/config/CorsFilter.java
+++ src/main/java/com/excella/reactor/config/CorsFilter.java
@@ -10,7 +10,9 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
+@Component
 @Slf4j
 public class CorsFilter implements Filter {
 
